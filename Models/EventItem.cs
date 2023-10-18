c706ea07687d16ccfb3901db901dbbc32a102765
Thomas Divine Smith II <thomasdivinesmith@gmail.com>
2023-10-18 16:28:58 -0400

    Added dark and gold styling

---
+++ Models/EventItem.cs
@@ -0,0 +1,19 @@
+namespace NBWSTech.Models;
+
+// EventItem.cs
+public class EventItem
+{
+    public string? Summary { get; set; }
+    public string? Start { get; set; }
+    public string? End { get; set; }
+    public string? Recurrence { get; set; }
+    public string? ColorID { get; set; }
+    public string? STARDescription { get; set; }
+    public string? Situation { get; set; }
+    public string? Task { get; set; }
+    public string? Action { get; set; }
+    public string? Result { get; set; }
+    public string? RecommendedDay { get; set; }
+    public string? DurationHours { get; set; }
+    public string? Priority { get; set; }
+}
